33cbde6c43035f2f17e0712513ce6dfaf0fba333
alialaa <hello@alialaa.com>
2025-05-14 11:30:09 +0300

    Lecture Code For custom-alias

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script>
+	import Button from '$components/button.svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<Button />
