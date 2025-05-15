affd586daa3ffd83996515a8c2024ea081ceaa0f
alialaa <hello@alialaa.com>
2025-05-15 10:38:28 +0300

    Lecture Code For creating-routes-and-navigating

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,12 @@
 	let { children } = $props();
 </script>
 
+<nav class="my-2 bg-amber-700 p-2">
+	<ul class="flex gap-2">
+		<li>
+			<a href="/">Home</a>
+			<a href="/about">About</a>
+		</li>
+	</ul>
+</nav>
 {@render children()}
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	let name = $state('NoteNow');
+</script>
+
+<h1 class="text-4xl">About {name}</h1>
+
+<style>
+	h1 {
+		color: var(--color-red-400);
+	}
+</style>
