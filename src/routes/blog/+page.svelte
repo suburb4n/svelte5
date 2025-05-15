d3a6f2f151999ac085091f820ab04953272b948c
alialaa <hello@alialaa.com>
2025-05-15 10:52:02 +0300

    Lecture Code For dynamic-route-params

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,1 @@
+<h1 class="text-4xl">Blog</h1>
---
+++ src/routes/blog/[id]/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { page } from '$app/state';
+
+	let pageId = $derived(page.params.id);
+</script>
+
+<h1 class="text-4xl">Single Blog: {pageId}</h1>
+<a href="/blog/123">Another Text</a>
