9a7a9b486496e3a5baa6775e6de659b11a008bfa
alialaa <hello@alialaa.com>
2025-05-15 11:44:19 +0300

    Lecture Code For layout-conditions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,23 @@
 <script lang="ts">
+	import { page } from '$app/state';
 	import '../app.css';
 
 	let { children } = $props();
 </script>
 
-<div class="border-2 bg-pink-900 p-4">
-	<h1 class="text-3xl">Root Layout</h1>
-	<nav class="my-2 bg-amber-700 p-2">
-		<ul class="flex gap-2">
-			<li>
-				<a href="/">Home</a>
-				<a href="/about">About</a>
-			</li>
-		</ul>
-	</nav>
+{#if page.url.pathname.startsWith('/about')}
 	{@render children()}
-</div>
+{:else}
+	<div class="border-2 bg-pink-900 p-4">
+		<h1 class="text-3xl">Root Layout</h1>
+		<nav class="my-2 bg-amber-700 p-2">
+			<ul class="flex gap-2">
+				<li>
+					<a href="/">Home</a>
+					<a href="/about">About</a>
+				</li>
+			</ul>
+		</nav>
+		{@render children()}
+	</div>
+{/if}
