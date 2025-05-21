c03daf8c1a83a036d46c4ecd680c329cecd307a2
alialaa <hello@alialaa.com>
2025-05-21 11:59:22 +0300

    Lecture Code For await-parent

--- src/routes/(marketing)/blog/+page.server.ts
+++ src/routes/(marketing)/blog/+page.server.ts
@@ -1,6 +1,10 @@
 import type { PageServerLoad } from './$types';
 
-export const load = (async () => {
+export const load = (async ({ parent }) => {
+	// DO stuff that does not depend on parent
+
+	const parentData = await parent();
+	console.log(parentData);
 	console.log('🌍 Blog Route Server Load');
 	return {
 		title: 'The Blog',
