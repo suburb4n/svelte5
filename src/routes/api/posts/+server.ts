ed5460387909b7aadc32ea077adcf63bd8f75147
alialaa <hello@alialaa.com>
2025-05-22 10:46:57 +0300

    Lecture Code For post-endpoint

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -1,9 +1,22 @@
-import { error, json } from '@sveltejs/kit';
+import { error, json, text } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async ({ fetch }) => {
 	const postsRes = await fetch('https://dummyjson.com/posts');
 	const postResJSON = await postsRes.json();
-	// return json(postResJSON, { status: 200 });
-	return error(401, 'Error');
+	return json(postResJSON, { status: postsRes.status });
+};
+
+export const POST: RequestHandler = async ({ request }) => {
+	const post = await request.json();
+
+	if (!post.title) {
+		error(400, 'Post title is required!');
+	}
+	// Insert post into db
+	return json({ id: crypto.randomUUID(), title: post.title });
+};
+
+export const fallback: RequestHandler = ({ request }) => {
+	return text(`${request.method} received!`);
 };
