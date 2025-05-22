2a452ccf347c4f5f2aafdbf52b99897410d11ba4
alialaa <hello@alialaa.com>
2025-05-22 12:12:25 +0300

    Lecture Code For blog-pagination

--- src/routes/(marketing)/blog/+page.server.ts
+++ src/routes/(marketing)/blog/+page.server.ts
@@ -1,10 +1,13 @@
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import type { PostsResponse } from '$lib/types';
+import { POSTS_PER_PAGE } from '$lib/constants';
 
-export const load = (async ({ fetch }) => {
-	// DO stuff that does not depend on parent
-	const postsRes = await fetch('/api/posts');
+export const load = (async ({ fetch, url }) => {
+	const page = +(url.searchParams.get('page') || 1);
+	const postsRes = await fetch(
+		`https://dummyjson.com/posts?limit=${POSTS_PER_PAGE}&skip=${(page - 1) * POSTS_PER_PAGE}`
+	);
 	if (!postsRes.ok) {
 		error(postsRes.status, 'An error has occurred!');
 	}
--- src/routes/(marketing)/blog/+page.svelte
+++ src/routes/(marketing)/blog/+page.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
+	import { page } from '$app/state';
+	import { POSTS_PER_PAGE } from '$lib/constants.js';
+
 	// import type { PageProps } from './$types';
 
 	let { data } = $props();
+
+	let currentPage = $derived(+(page.url.searchParams.get('page') ?? 1));
 </script>
 
 <div class="container mx-auto px-4 py-8">
@@ -12,5 +17,15 @@
 		{#each data.posts.posts as post}
 			<data.component {post} />
 		{/each}
+	</div>
+	<div class="flex justify-end">
+		<div class="join mt-10 grid w-full max-w-[500px] grid-cols-2 gap-4">
+			{#if currentPage > 1}
+				<a href="/blog?page={currentPage - 1}" class="join-item btn btn-outline">Previous page</a>
+			{/if}
+			{#if Math.ceil(data.posts.total / POSTS_PER_PAGE) !== currentPage}
+				<a href="/blog?page={currentPage + 1}" class="join-item btn btn-outline">Next</a>
+			{/if}
+		</div>
 	</div>
 </div>
