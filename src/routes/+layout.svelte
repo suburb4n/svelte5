9d647d5870faebcdd7ef5ee68ece9d15d69f9b4e
alialaa <hello@alialaa.com>
2025-05-24 10:13:21 +0300

    post seo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,20 +6,14 @@
 </script>
 
 <svelte:head>
-	{#if page.data.post}
-		<title>{page.data.post.title}</title>
-		<meta property="og:title" content={page.data.post.title} />
-		<meta property="og:description" content={page.data.post.body.slice(0, 200)} />
-	{:else}
-		<title>{page.data.title ? `${page.data.title} | NoteNow` : 'NoteNow'}</title>
-		<meta
-			property="og:title"
-			content={page.data.title ? `${page.data.title} | NoteNow` : 'NoteNow'}
-		/>
-		{#if page.data.description}
-			<meta property="og:description" content={page.data.description} />
-			<meta name="description" content={page.data.description} />
-		{/if}
+	<title>{page.data.title ? `${page.data.title} | NoteNow` : 'NoteNow'}</title>
+	<meta
+		property="og:title"
+		content={page.data.title ? `${page.data.title} | NoteNow` : 'NoteNow'}
+	/>
+	{#if page.data.description}
+		<meta property="og:description" content={page.data.description} />
+		<meta name="description" content={page.data.description} />
 	{/if}
 </svelte:head>
 
