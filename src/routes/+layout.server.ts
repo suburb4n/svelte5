4113d93e51e32b9a73d433b1808806277cbf7cc4
alialaa <hello@alialaa.com>
2025-05-28 11:29:39 +0300

    Lecture Code For protecting-routes-with-layouts

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,11 @@
+import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
 
-export const load = (async ({ cookies }) => {
+export const load = (async ({ cookies, route }) => {
 	const token = cookies.get('token');
+	if (!token && route.id?.startsWith('/(app)')) {
+		redirect(307, '/signin');
+	}
 	return {
 		user: token ? { name: 'John', id: 1 } : null
 	};
