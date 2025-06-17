f5384847116d0c449439bdf376861a5f37b0d37e
alialaa <hello@alialaa.com>
2025-06-17 11:05:59 +0300

    Lecture Code For form-actions-create-workspace

--- src/routes/(app)/new/+page.server.ts
+++ src/routes/(app)/new/+page.server.ts
@@ -1,5 +1,8 @@
 import { fail, redirect } from '@sveltejs/kit';
 import type { Actions, PageServerLoad } from './$types';
+import { db } from '$lib/server/db';
+import { roles, workspaceAccess, workspaces } from '$lib/server/db/schema';
+import { eq } from 'drizzle-orm';
 
 export const load = (async ({ locals }) => {
 	if (!locals.session) {
@@ -21,9 +24,31 @@ export const actions = {
 		if (name.toString().length < 4) {
 			return fail(400, { message: 'Name is too short!', name });
 		}
-
-		// Create Workspace
-
-		return { message: 'Workspace created successfully!' };
+		let _newWorkspace;
+		try {
+			_newWorkspace = await db.transaction(async (tx) => {
+				const [newWorkspace] = await tx
+					.insert(workspaces)
+					.values({
+						name: name.toString()
+					})
+					.returning({ id: workspaces.id });
+				if (!newWorkspace) throw new Error('Workspace creation failed!');
+				const [adminRole] = await tx.select().from(roles).where(eq(roles.name, 'admin'));
+				if (!adminRole) throw new Error('Admin role not found');
+				await tx.insert(workspaceAccess).values({
+					userId: locals.session.user.id,
+					workspaceId: newWorkspace.id,
+					roleId: adminRole.id
+				});
+				return newWorkspace;
+			});
+		} catch (error) {
+			// Report
+			console.log(error);
+			return fail(500, { message: 'An error has occurred!' });
+		}
+		redirect(303, `/w/${_newWorkspace.id}`);
+		// return { message: 'Workspace created successfully!' };
 	}
 } satisfies Actions;
