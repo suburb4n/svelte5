b81057c8b84a2ac41d7ae7c38629b138dd9aa2cf
alialaa <hello@alialaa.com>
2025-06-24 10:53:15 +0300

    Lecture Code For ws-access-query

--- src/routes/(app)/(workspace)/w/[wid]/+page.server.ts
+++ src/routes/(app)/(workspace)/w/[wid]/+page.server.ts
@@ -1,13 +1,24 @@
 import { fail, redirect } from '@sveltejs/kit';
 import type { PageServerLoad, Actions } from './$types';
 import { db } from '$lib/server/db';
-import { workspaces } from '$lib/server/db/schema';
-import { eq } from 'drizzle-orm';
+import { roles, workspaceAccess, workspaces } from '$lib/server/db/schema';
+import { and, eq } from 'drizzle-orm';
 import { requireLogin } from '$lib/utils';
 
-export const load = (async () => {
+export const load = (async ({ params }) => {
 	// TODO: Page Auth
-	requireLogin();
+	const { user } = requireLogin();
+	const [wsAccess] = await db
+		.select({
+			workspaceId: workspaceAccess.workspaceId,
+			role: roles.name,
+			roldeId: roles.id
+		})
+		.from(workspaceAccess)
+		.innerJoin(roles, eq(workspaceAccess.roleId, roles.id))
+		.where(and(eq(workspaceAccess.workspaceId, params.wid), eq(workspaceAccess.userId, user.id)))
+		.limit(1);
+	console.log('wsAccess', wsAccess);
 	return {};
 }) satisfies PageServerLoad;
 
